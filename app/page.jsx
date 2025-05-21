da44e36e52cf8c92edee196562e0deef9037f193
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2025-05-21 21:54:14 +0600

    Side bar updated

---
+++ app/page.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { assets } from "@/assets/assets";
+import Sidebar from "@/components/sideBar";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [expand, setExpand] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div>
+      <div className="flex h-screen">
+        <Sidebar expand={expand} setExpand={setExpand}/>
+        <div className="flex-1 flex flex-col justify-center items-center px-4 pb-8 text-white relative bg-[#292a2d]">
+          <div className="md:hidden absolute px-4 top-6 flex justify-between items-center w-full">
+            {assets.menuIcon && (
+              <Image onClick={()=> (expand ? setExpand(false) : setExpand(true))}
+                className="rotate-180"
+                src={assets.menuIcon}
+                alt="menu"
+                width={24}
+                height={24}
+              />
+            )}
+            {assets.chatIcon && (
+              <Image
+                className="Opacity-70"
+                src={assets.chatIcon}
+                alt="chat"
+                width={24}
+                height={24}
+              />
+            )}
+          </div>
+          {messages.length == 0 ? (
+            <>
+            <div>
+               <Image
+              className="w-24 h-24"
+              src={assets.chatIcon}
+              alt="chat"
+              width={96}
+              height={96}/>
+            <h1 className="text-2xl font-bold mt-4">Hi, Im Deepseek</h1>
+            </div>
+            <p>How Can I help you?</p>
+            </>
+          ):(
+          <div>
+            
+          </div>
+          
+          )
+        }
+        <p className="text-xs fixed absloute bottom-1 text-gray-500"> Ai Generated for Reference Only</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ assets/assets.js
@@ -0,0 +1,23 @@
+import rightArrow from "./right-arrow.svg";
+import leftArrow from "./left-arrow.svg";
+import menuIcon from "./menu.svg";
+import search from "./search.svg";
+import chatIcon from "./chat-icon.svg";
+import deepSeek from "./deepseek-color.svg";
+import deepSeekText from "./deepseek-text.svg";
+import sideBarLeft from "./sidebar-left.svg";
+import sideBarRight from "./sidebar-right.svg";
+
+
+export const assets = {
+  rightArrow,
+  leftArrow,
+  menuIcon,
+  search,
+  chatIcon,
+  deepSeek,
+  deepSeekText,
+  sideBarLeft,
+  sideBarRight,
+
+};
